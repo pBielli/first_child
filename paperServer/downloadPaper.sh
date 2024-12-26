88981aa34e0fac50b846f8b481760cb8a19a3b81
pBielli <pat@firstchild.router>
2024-12-26 12:35:01 +0100

    .

--- paperServer/downloadPaper.sh
+++ paperServer/downloadPaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Scarica Paper
-wget -O paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/416/downloads/paper-1.20.4-416.jar
-#wget -O paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.1/builds/157/downloads/paper-1.20.1-157.jar
+#wget -O paper.jar https://api.papermc.io/v2/projects/paper/versions/1.20.4/builds/416/downloads/paper-1.20.4-416.jar
+wget -O paper.jar https://api.papermc.io/v2/projects/paper/versions/1.21.3/builds/82/downloads/paper-1.21.3-82.jar
 
 mkdir /app
 mkdir ~/paper-data
